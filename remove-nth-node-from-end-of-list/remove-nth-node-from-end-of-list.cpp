c743e780100e61e30e0482cf344f17d003103f96
Kang Da Sol <dskang070@naver.com>
2021-11-04 14:41:33 +0900

    Time: 8 ms (28.19%), Space: 10.8 MB (31.04%) - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -13,12 +13,12 @@ public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode *tmpNode = head;
         int size;
+        
         for (size = 0; tmpNode != NULL; size++) {
             tmpNode = tmpNode->next;
         }
-        //printf("linked list size : %d\n", size);
+        
         tmpNode = head;
-        //printf("%d\n", tmpNode->val);
         
         ListNode *prev = NULL;
         ListNode *after = NULL;
@@ -27,18 +27,17 @@ public:
             prev = tmpNode;
             tmpNode = tmpNode->next;
             after = tmpNode->next;
-            //printf("prev : %d / after : %d\n", prev->val, after->val);
         }
         
         if (prev != NULL) {
-            //printf ("%d %d, ", prev->val, after->val);
             prev->next = after;
         } else {
             head = tmpNode->next;
         }
         
-        if ((size == 1) && (n == 1)) return NULL;
-        
+        if ((size == 1) && (n == 1)) {
+            return NULL;
+        }
         return head;
     }
 };
