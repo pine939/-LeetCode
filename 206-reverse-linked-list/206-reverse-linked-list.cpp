32ff9cce07894abcbbaa82d3e8cf8c264232444a
Kang Da Sol <dskang070@naver.com>
2022-02-24 22:30:03 +0900

    Time: 10 ms (41.51%), Space: 8.1 MB (97.53%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -14,11 +14,11 @@ public:
         ListNode *answer = NULL;
         ListNode *cur = head;
         
-        while (cur != NULL) {
-            ListNode *tmp = cur->next;
-            cur->next = answer;
-            answer = cur;
-            cur = tmp;
+        while (head != NULL) {
+            ListNode *tmp = head->next;  // 다음 주소를 임시로 가지고 있는다.
+            head->next = answer;  // 다음 주소가 answer를 가리키도록 (맨 처음은 NULL이다)
+            answer = head;  // answer는 리턴해야 하는 reverse값. 현재 값을 가리키도록 한다.
+            head = tmp;  // 다음 반복을 위함
         }
         
         return answer;
