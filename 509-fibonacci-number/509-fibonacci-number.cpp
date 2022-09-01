067dddbfec8e10fc4f51d77a8bfeab4b6c002e08
Kang Da Sol <dskang070@naver.com>
2022-09-01 12:38:22 +0900

    Time: 0 ms (100.00%), Space: 5.8 MB (96.08%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     int fib(int n) {
+        /* slow... 
         if (n <= 1) return n;
         if (n == 2) return 1;
         
         return (fib(n-1) + fib(n-2));
+        */
+        
+        // faster
+        int arr[31] = {0, 1, 0};
+        for (int i = 2; i <= 30; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
     }
 };
