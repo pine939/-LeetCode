255588e92e340d89280fb8f1b531c0dd043c082d
Kang Da Sol <dskang070@naver.com>
2022-09-11 18:57:44 +0900

    Time: 56 ms (5.61%), Space: 21.7 MB (30.75%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -16,21 +16,42 @@ public:
       if (root == nullptr) {
           return true;
       }
-      
+      std::cout << "root->val: " << root->val << std::endl;
+      std::cout << "min: " << min << std::endl;
+      std::cout << "max: " << max << std::endl;
+      std::cout << "------" << std::endl;
       if (root->val <= min || root->val >= max) {
           return false;
       }
       
-      return (isValidBST(root->left, min, root->val) && isValidBST(root->right, root->val, max));
+      return isValidBST(root->left, min, root->val) && isValidBST(root->right, root->val, max);
   }
 };
 
-      /*
-      std::cout << root->val << std::endl;
-      std::cout << min << std::endl;
-      std::cout << max << std::endl;
-      
-      
-      std::cout << root->left->val << std::endl;
-      std::cout << root->right->val << std::endl;
-      */
+
+/*
+1. TreeNode를 vector에 넣는다. 
+2. vector를 순회하면서 판단한다.
+
+///
+
+min, max로 뭔가 넘겨서 비교한다...?
+
+root->val vs min
+root->val vs max 
+
+return false되는 상황
+root->val > max
+root->val < min
+
+if (root == nullptr) {
+  return true;
+}
+
+if (root->val <= min || root->val >= max) {
+  return false;
+}
+
+return isValidBST(root->left, min, root->val) && isValidBST(root->right, root->val, max);
+
+*/
